4a084d404c34975523c201c212bd951c15e763fc
Renata <renatab_leao@yahoo.com.br>
2022-06-12 11:16:16 -0300

    Fixing bugs

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure SQLite to read database
-con = sqlite3.connect("postgres://cgrclmtuxjcmql:57fa3009cd53b1c524510bfdf20b463cb5d11e64180c3ca494ecc6d02d94ad53@ec2-54-165-178-178.compute-1.amazonaws.com:5432/d7bcgerk5e2tnt", check_same_thread=False)
+#con = sqlite3.connect("postgres://cgrclmtuxjcmql:57fa3009cd53b1c524510bfdf20b463cb5d11e64180c3ca494ecc6d02d94ad53@ec2-54-165-178-178.compute-1.amazonaws.com:5432/d7bcgerk5e2tnt", check_same_thread=False)
 # con = sqlite3.connect("finalproject.db", check_same_thread=False)
 db = con.cursor()
 
